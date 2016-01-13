3196bb5f73f899bb1cf8463a933ad58fa8710974
Abdiel Jaramillo Ojedis <abdielja@gmail.com>
2016-01-13 05:24:24 -0500

    Call to user remote service added (Not working yet). UserRestService.

---
+++ sci-service-hub/src/main/java/com/sci/servicehub/service/remote/UserRestService.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.sci.servicehub.service.remote;
+
+import org.springframework.web.client.RestTemplate;
+
+import com.sci.servicehub.model.UserData;
+
+/**
+ * @author Abdiel Jaramillo
+ *
+ */
+
+public class UserRestService
+{
+
+    // ***** Constructors *****
+    
+    public UserRestService()
+    {
+     
+    }
+
+    
+    // ***** Public Methods *****
+    
+    public UserData GetUserData()
+    {
+     
+        UserData ud = null;
+
+        RestTemplate restTemplate = new RestTemplate();
+        RestResponse res = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", RestResponse.class);
+        
+        return ud;
+        
+    }
+    
+}
